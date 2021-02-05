fa6e090052368fc0248d190d56e59eaf5b419572
theprismdata <theprismdata@gmail.com>
2021-02-05 11:01:09 +0900

    설비 코드 자동 설정 키 추가

--- iiot_server.py
+++ iiot_server.py
@@ -92,18 +92,147 @@ class TcpServer:
             redis_con = self.connect_redis(address, port)
             facilities_dict = redis_con.get('facilities_info')
             if facilities_dict is None:
-                facilities_dict = {'TS0001': {
-                    '0001': 'TS_VOLT1_(RS)',
-                    '0002': 'TS_VOLT1_(ST)',
-                    '0003': 'TS_VOLT1_(RT)',
-                    '0004': 'TS_AMP1_(R)',
-                    '0005': 'TS_AMP1_(S)',
-                    '0006': 'TS_AMP1_(T)',
-                    '0007': 'INNER_PRESS',
-                    '0008': 'PUMP_PRESS',
-                    '0009': 'TEMPERATURE1(PV)',
-                    '0010': 'TEMPERATURE1(SV)',
-                    '0011': 'OVER_TEMP'}
+                facilities_dict = {
+                    'TS0001':
+                    {
+                                '0001': 'TS_VOLT1_(RS)',
+                                '0002': 'TS_VOLT1_(ST)',
+                                '0003': 'TS_VOLT1_(RT)',
+                                '0004': 'TS_AMP1_(R)',
+                                '0005': 'TS_AMP1_(S)',
+                                '0006': 'TS_AMP1_(T)',
+                                '0007': 'INNER_PRESS',
+                                '0008': 'PUMP_PRESS',
+                                '0009': 'TEMPERATURE1(PV)',
+                                '0010': 'TEMPERATURE1(SV)',
+                                '0011': 'OVER_TEMP'
+                    },
+                'TS0002':
+                    {
+                                '0001': 'TS_VOLT1_(RS)',
+                                '0002': 'TS_VOLT1_(ST)',
+                                '0003': 'TS_VOLT1_(RT)',
+                                '0004': 'TS_AMP1_(R)',
+                                '0005': 'TS_AMP1_(S)',
+                                '0006': 'TS_AMP1_(T)',
+                                '0007': 'INNER_PRESS',
+                                '0008': 'PUMP_PRESS',
+                                '0009': 'TEMPERATURE1(PV)',
+                                '0010': 'TEMPERATURE1(SV)',
+                                '0011': 'OVER_TEMP'
+                    },
+                'TS0003':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0004':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0005':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0006':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0007':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0008':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS0009':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
+                'TS9999':
+                    {
+                        '0001': 'TS_VOLT1_(RS)',
+                        '0002': 'TS_VOLT1_(ST)',
+                        '0003': 'TS_VOLT1_(RT)',
+                        '0004': 'TS_AMP1_(R)',
+                        '0005': 'TS_AMP1_(S)',
+                        '0006': 'TS_AMP1_(T)',
+                        '0007': 'INNER_PRESS',
+                        '0008': 'PUMP_PRESS',
+                        '0009': 'TEMPERATURE1(PV)',
+                        '0010': 'TEMPERATURE1(SV)',
+                        '0011': 'OVER_TEMP'
+                    },
                 }
                 redis_con.set('facilities_info', json.dumps(facilities_dict))
 
