fc439481615cf07ce039b7231ba6b1dbb653a100
신홍중 <theprismdata@gmail.com>
2021-04-17 13:23:33 +0900

    msg debug

--- net_socket/iiot_tcp_async_server.py
+++ net_socket/iiot_tcp_async_server.py
@@ -216,7 +216,8 @@ class AsyncServer:
         try:
             byte_tuple = self.convert(list(packet_bytes))
             logging.debug('byte message')
-            logging.debug('['+str(byte_tuple)+']')
+            logging.debug('1[' + packet_bytes+ ']')
+            logging.debug('2['+str(byte_tuple)+']')
             if byte_tuple[0] == 2 and (byte_tuple[16] == 3 or byte_tuple[18] == 3):
                 group = chr(byte_tuple[5]) + chr(byte_tuple[6])
                 group_code = int('0x{:02x}'.format(byte_tuple[7]) + '{:02x}'.format(byte_tuple[8]), 16)
