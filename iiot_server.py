b303b9e28b4c2e8bb85ee8b86dd4f352a1aff8db
신홍중 <theprismdata@gmail.com>
2021-06-03 16:25:43 +0900

    server socket option => SO_REUSEADDR

--- iiot_server.py
+++ iiot_server.py
@@ -343,6 +343,7 @@ class TcpServer:
     def get_server_socket(self):
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.setblocking(0)
             server_socket.bind(('', self.tcp_port))
             server_socket.listen(1)
